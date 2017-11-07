142c3dd8ba05488976ebdb02b2fec5b2063b105d
Gavin Sidebottom <gsidebo@mit.edu>
2017-11-07 14:23:26 -0500

    Changed 'queue' app name to 'queue_app' to avoid name collisions

--- xqueue/test_settings.py
+++ xqueue/test_settings.py
@@ -91,5 +91,5 @@ NOSE_ARGS = ['--cover-erase', '--with-xunit', '--with-xcoverage',
              '--cover-html',
              '--cover-inclusive', '--cover-html-dir',
              os.environ.get('NOSE_COVER_HTML_DIR', 'cover_html'),
-             '--cover-package', 'queue', 'queue']
+             '--cover-package', 'queue_app', 'queue_app']
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
